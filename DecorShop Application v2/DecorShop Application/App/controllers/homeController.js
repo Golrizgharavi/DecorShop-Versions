3c2c93b26d942f364b99d2e2921870ae7d4ba4c2
Golriz <golriz.gharavi@icloud.com>
2019-02-17 19:07:56 -0500

    Request Form has been added
    Footer has been added
    Menu updated
    to do:
    set up back end Sql and C# to save orders
    control panel

---
+++ DecorShop Application v2/DecorShop Application/App/controllers/homeController.js
@@ -0,0 +1,21 @@
+﻿
+'use strict'
+app.controller('homeController', function (shopService) {
+
+    var vm = this;
+
+    vm.message = 'Golriz';
+    vm.SendOrder = function () {
+
+        var oreder = {
+
+            name: vm.Name,
+            email: vm.Email,
+            phone: vm.Phone,
+            order: vm.OContent
+        }
+        alert(JSON.stringify(oreder))
+    }
+
+
+})
---
+++ DecorShop Application v2/DecorShop Application/App/services/shopService.js
@@ -0,0 +1,5 @@
+﻿
+app.service('shopService', function ($http) {
+
+
+})
